bb2b0c9cffb6caf649fb97fb8f35bb893e73bce5
Hanseul Jun <hanseuljun@gmail.com>
2022-07-30 14:55:18 -0700

    Rebuild iphonesimulator which seems like working.

--- scripts/configure-iphonesimulator.sh
+++ scripts/configure-iphonesimulator.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+
+export CC='xcrun --sdk iphonesimulator clang'
+export CXX='xcrun --sdk iphonesimulator clang++'
+export EXTRA_CFLAGS="-arch arm64 -mios-version-min=14.0 -isysroot $(xcrun --sdk iphonesimulator --show-sdk-path)"
+
 ../libvpx/configure --target=generic-gnu --prefix=$(dirname $(pwd))/1.10.0/arm64-iphonesimulator
