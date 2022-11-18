c7761b608f2469028725c4d2ac18c2b6dc675c42
Hanseul Jun <hanseuljun@gmail.com>
2022-11-17 17:55:35 -0800

    Add linux build support.

--- build.py
+++ build.py
@@ -80,12 +80,30 @@ def build_arm64_iphonesimulator_binaries():
     subprocess.run(["make", "-C", build_path, "install"], check=True)
 
 
+def build_x64_linux_binaries():
+    here = Path(__file__).parent.resolve()
+    build_path = f"{here}/build/x64-linux"
+    if not os.path.exists(build_path):
+        os.makedirs(build_path)
+
+    subprocess.run([f"{here}/libvpx/configure",
+                    "--target=x86_64-linux-gcc",
+                    f"--prefix={here}/install/x64-linux"],
+                   cwd=build_path,
+                   check=True)
+    subprocess.run(["make", "-C", build_path, "-j8"], check=True)
+    subprocess.run(["make", "-C", build_path, "install"], check=True)
+
+
 def main():
     if platform.system() == "Darwin":
         build_arm64_mac_binaries()
         build_x64_mac_binaries()
         build_arm64_ios_binaries()
         build_arm64_iphonesimulator_binaries()
+        return
+    elif platform.system() == "Linux":
+        build_x64_linux_binaries()
         return
 
     raise Exception(f"libvpx build not supported.")
