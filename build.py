a151f04d4b90818976e6c81833087556555c0233
Hanseul Jun <hanseuljun@gmail.com>
2022-12-05 14:06:51 -0800

    Add windows build to build.py.

--- build.py
+++ build.py
@@ -5,6 +5,42 @@ import subprocess
 from pathlib import Path
 
 
+def find_msys64_env():
+    path1 = "c:/msys64/usr/bin/env.exe"
+    if os.path.exists(path1):
+        return path1
+    path2 = "c:/tools/msys64/usr/bin/env.exe"
+    if os.path.exists(path2):
+        return path2
+    return None
+    
+
+def run_in_mingw(extra_args, check=False):
+    msys64_env_path = find_msys64_env()
+    if msys64_env_path == None:
+        raise "No msys64 env"
+
+    args = [msys64_env_path, "MSYSTEM=MINGW64"]
+    args = args + extra_args
+    subprocess.run(args,
+                   check=check)
+
+
+def build_x64_windows_binaries():
+    here = Path(__file__).parent.resolve()
+    build_path = f"{here}/build/x64-windows"
+    if not os.path.exists(build_path):
+        os.makedirs(build_path)
+    mingw_here = str(here).replace("\\", "/")
+
+    run_in_mingw(["/bin/bash",
+                  "--login",
+                  f"{here}/mingw_build.sh",
+                  mingw_here,
+                  build_path],
+                  check=True)
+
+
 def build_arm64_mac_binaries():
     here = Path(__file__).parent.resolve()
     build_path = f"{here}/build/arm64-mac"
@@ -96,7 +132,10 @@ def build_x64_linux_binaries():
 
 
 def main():
-    if platform.system() == "Darwin":
+    if platform.system() == "Windows":
+        build_x64_windows_binaries()
+        return
+    elif platform.system() == "Darwin":
         build_arm64_mac_binaries()
         build_x64_mac_binaries()
         build_arm64_ios_binaries()
---
+++ mingw_build.sh
@@ -0,0 +1,7 @@
+set -e
+
+pushd $2
+"$1/libvpx/configure" --target=x86_64-win64-gcc "--prefix=$1/install/x64-windows"
+make -j8
+make install
+pushd $2
